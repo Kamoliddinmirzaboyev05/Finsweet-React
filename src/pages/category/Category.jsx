b212742bec822a995fefa63314d4a6d813166584
Kamoliddin Mirzaboyev <Kamoliddinmirzaboyev05@gmail.com>
2025-02-25 11:51:08 +0500

    Update

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -28,67 +28,22 @@ function Category({ categories }) {
         <section className="mainContent">
           <div className="container">
             <div className="blogPosts">
-              <div className="blogPost">
-                <div className="postImg">
-                  <img src="/categorypost.png" alt="" />
-                </div>
-                <div className="postText">
-                  <p className="postType">STARTUP</p>
-                  <h2>Top 6 free website mockup tools 2022</h2>
-                  <p className="postInfo">
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident.
-                  </p>
-                </div>
-              </div>
-
-              <div className="blogPost">
-                <div className="postImg">
-                  <img src="/categorypost.png" alt="" />
-                </div>
-                <div className="postText">
-                  <p className="postType">STARTUP</p>
-                  <h2>Logo design trends to avoid in 2022 </h2>
-                  <p className="postInfo">
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident.
-                  </p>
-                </div>
-              </div>
-              <div className="blogPost">
-                <div className="postImg">
-                  <img src="/categorypost.png" alt="" />
-                </div>
-                <div className="postText">
-                  <p className="postType">TECHNOLOGY</p>
-                  <h2>
-                    8 Figma design systems you can download for free today{" "}
-                  </h2>
-                  <p className="postInfo">
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident.
-                  </p>
-                </div>
-              </div>
-              <div className="blogPost">
-                <div className="postImg">
-                  <img src="/categorypost.png" alt="" />
-                </div>
-                <div className="postText">
-                  <p className="postType">ECONOMY</p>
-                  <h2>
-                    Font sizes in UI design: The complete guide to follow{" "}
-                  </h2>
-                  <p className="postInfo">
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident.
-                  </p>
-                </div>
-              </div>
+              {oneProduct[0].post.map((item) => {
+                return (
+                  <div className="blogPost">
+                    <div className="postImg">
+                      <img src="/categorypost.png" alt="" />
+                    </div>
+                    <div className="postText">
+                      <p className="postType">{item.type}</p>
+                      <h2>{item.title}</h2>
+                      <p className="postInfo">
+                       {item.info}
+                      </p>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
             <div className="leftSide">
               <div className="categories">
